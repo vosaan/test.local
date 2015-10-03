5da8ea8741dbb3e12769ef91686a733cf92947b5
vosaan <vo.saan@gmail.com>
2015-10-03 21:57:04 +0300

    Parser

--- themes/theme01/index.php
+++ themes/theme01/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/model/model.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/libs/simpledom/simple_html_dom.php');
 $allFeeds = getFeedbacks($link);
 include_once ($_SERVER['DOCUMENT_ROOT'].'/config/smarty_init.php');
 include_once ($_SERVER['DOCUMENT_ROOT'].'/index.php');
@@ -17,4 +18,23 @@ $smarty->assign('mainmenu', $mainmenu);
 $smarty->assign('feed', $allFeeds);
 /*$gettime = time();
 $smarty->assign('datetime', $gettime);*/
+
+$arr = array();
+$html = file_get_html('http://www.gismeteo.ua/city/daily/5093/');
+
+foreach($html->find('#tbwdaily1 tr') as $element){
+	$arr[] = trim($element->innertext);
+}
+$html->clear(); 
+unset($html);
+
+$str = preg_split('/<[^>]*[^\/]>/i', $arr[0], -1, PREG_SPLIT_NO_EMPTY);
+$arr2 = array();
+foreach($str as $rec){
+		if(($rec)!=""){
+			$arr2[] = $rec;
+		}
+}
+$smarty->assign('today', $arr);
+
 ?>
