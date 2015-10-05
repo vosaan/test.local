ec0f01e98aac53fd912eed6b20b385c7098e608d
vosaan <vo.saan@gmail.com>
2015-10-05 18:27:00 +0300

    parsing

--- themes/theme01/index.php
+++ themes/theme01/index.php
@@ -1,46 +1,56 @@
 <?php
+include_once ($_SERVER['DOCUMENT_ROOT'].'/config/smarty_init.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/model/model.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/libs/simpledom/simple_html_dom.php');
-$allFeeds = getFeedbacks($link);
-include_once ($_SERVER['DOCUMENT_ROOT'].'/config/smarty_init.php');
-//include_once ($_SERVER['DOCUMENT_ROOT'].'/index.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/menu/mainmenu.php');
+
 if (isset($_SESSION['isLogin']) && isset($_SESSION['username'])){
 	$smarty->assign('login', $_SESSION['isLogin']);
 	$smarty->assign('username', $_SESSION['username']);
 }
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/menu/mainmenu.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/index.php');
-
 $smarty->assign('mainmenu', $mainmenu);
+
+$allFeeds = getFeedbacks($link);
 $smarty->assign('feed', $allFeeds);
 
-$today = parseWeather('http://www.gismeteo.ua/city/daily/5093/', '#tbwdaily1 tr td');
-$smarty->assign('today', $today);
+$html = file_get_html('https://www.gismeteo.ua/weather-zaporizhzhya-5093/');
 
-/*$gettime = time();
-$smarty->assign('datetime', $gettime);*/
-
-//$arr = array();
-//$html = file_get_html('http://www.gismeteo.ua/city/daily/5093/');
-
-//foreach($html->find('#tbwdaily1 tr td') as $element){
-//	if($element->innertext != ""){
-//		$arr[] = trim($element->innertext);
-//	}	
-//}
-//$html->clear(); 
-//unset($html);
-
-//$arr = array_diff($arr, array(''));
-
-/*$str = preg_split('/<[^>]*[^\/]>/i', $arr[0], -1, PREG_SPLIT_NO_EMPTY);
-$arr2 = array();
-foreach($str as $rec){
-		if(($rec)!=""){
-			$arr2[] = $rec;
-		}
+foreach($html->find('th') as $element){
+	if(isset($element->title)){
+		$arr[] = $element->title.'::'.trim($element);
+	} 
 }
-$smarty->assign('today', $arr);
 
-?>*/
+
+foreach($html->find('tr.wrow td') as $element){
+	if(!$element -> img){
+		$arr2[] = $element->plaintext;
+	} 
+}
+
+//$arr2 = array_diff($arr2, array(''));
+//$arr2 = array_values(array_filter($arr2));
+for($i=sizeof($arr)-1;$i>=0;$i--)
+{
+  $arr[$i+7] = $arr[$i];
+}
+//unset($arr[0]);
+//array_unshift($arr2, $arr[0]); 
+
+?><pre>
+	<?
+	/*$j=0;
+	//echo count($arr2);
+	for($i=0; $i<count($arr); $i++){
+		for(; $j<count($arr2); $j+=6){
+			//$arr[$j] = $arr[$i];
+			echo $i."  ".$j; continue; continue;
+		}
+	}
+	//print_r($arr);
+	//print_r(array_chunk($arr2, 6));*/
+	print_r($arr);	
+	?>
+
+
