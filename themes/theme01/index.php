102de7a222a74253f8c8d2804c272bf7701ef47a
vosaan <vo.saan@gmail.com>
2015-10-07 12:29:36 +0300

    вроде работает как надо

--- themes/theme01/index.php
+++ themes/theme01/index.php
@@ -16,37 +16,78 @@ $smarty->assign('feed', $allFeeds);
 
 $html = file_get_html('https://www.gismeteo.ua/weather-zaporizhzhya-5093/');
 
+/**
+ * Функция "отсекает" все символы в строке после заданного (используется для того,
+ * чтобы отделить показатели погоды в метрической системе и отсеч показатели в другх системах)
+ */
 function delete($str,$symbol=' ') 
 {
 	return($strpos=mb_strpos($str,$symbol))!==false?mb_substr($str,0,$strpos,'utf8'):$str;
 } 
 
+/**
+ *В массив $arr_times_of_day заносятся значения времени суток за 3 дня.
+ */
 foreach($html->find('th') as $element){
 	if(isset($element->title)){
-		$arr[] = trim($element->plaintext);
+		$arr_times_of_day[] = trim($element->plaintext);
 	} 
 }
-for($i=0, $k=0; $i<=77, $k<=11; $i+=7, $k++) {
-		$arrn[$i]=$arr[$k];
-}
 
-
+/**
+ *В масив $arr_weather заносятся показатели погоды за 3 дня (только в мерической системе). В массиве
+ *присутствуют пустые элементы, на их месте должны быть картинки ("ясно", "облачно" и т.д.), которые
+ *plaintext пропускает. Поскольку эти картинки дублируются текстом, то на их место будут занесены
+ *значения времени суток
+ */
 foreach($html->find('tr.wrow td') as $element){
 	if(!$element -> img){
-		$arr2[] = delete($element->plaintext, " ");
+		$arr_weather[] = delete($element->plaintext, " ");
 	} 
 }
 
-for($j=0; $j<=77; $j+=7){
-	$arr2[$j]=$arrn[$j];
+/**
+ *В массив $arr_times_of_day_nums заносятся элементы из массива $arr_times_of_day, но с изменёнными
+ *индексами. Индексы заменены таким образом, что при занесении в $arr_weather значений из $arr_times_of_day_nums
+ *в пустые элементы массива с показателями погоды занесутся значения времени суток
+ */
+for($i = 0, $j = 0; $i <= count($arr_weather)-1, $j <= count($arr_times_of_day)-1; $i += 7, $j++) {
+		$arr_times_of_day_nums[$i]=$arr_times_of_day[$j];
 }
 
-//$arr2 = array_diff($arr2, array(''));
-//$arr2 = array_values(array_filter($arr2));
+/**
+ *Занесение в $arr_weather значений из $arr_times_of_day_nums
+ */
+for($i = 0; $i <= count($arr_weather)-1; $i += 7){
+	$arr_weather[$i]=$arr_times_of_day_nums[$i];
+}
 
+/**
+ *Разнесение показателей погоды на 3 массива ("сегодня", "завтра", "послезавтра")
+ */
+$today = array_slice($arr_weather, 0, 28);
+$tomorrow = array_slice($arr_weather, 28, 28);
+$after_tomorrow = array_slice($arr_weather, 56, 28);
 
-//unset($arr[0]);
-//array_unshift($arr2, $arr[0]); 
+/**
+ *Определение дат ("сегодня", "завтра", "послезавтра")
+ */
+setlocale(LC_TIME, "ru-RU");
+$today_date = strftime("%A, ".'%d'.'.'.'%m'.'.'.'%Y');
+$tomorrow_date = strftime("%A, ".'%d'.'.'.'%m'.'.'.'%Y',strtotime("+1 day"));
+$after_tomorrow_date = strftime("%A, ".'%d'.'.'.'%m'.'.'.'%Y',strtotime("+2 day"));
+
+/**
+ *Определение переменных Smarty для вывода вседений о погоде
+ */
+$smarty->assign('today_date', $today_date);
+$smarty->assign('tomorrow_date', $tomorrow_date);
+$smarty->assign('after_tomorrow_date', $after_tomorrow_date);
+
+$smarty->assign('arr_today', $today);
+$smarty->assign('arr_tomorrow', $tomorrow);
+$smarty->assign('arr_after_tomorrow', $after_tomorrow);
+ 
 ?>
 	<?
 	/*$j=0;
@@ -58,11 +99,25 @@ for($j=0; $j<=77; $j+=7){
 		}
 	}
 	//print_r($arr);*/
-	$arr_new = array_chunk($arr2, 7);
-	$smarty->assign('weath', $arr_new);
-	//print_r($arr_new);	
+	//$arr_new = array_chunk($arr2, 7);
+	//$arr_day = array_chunk($arr_new, 3);
+	//$smarty->assign('weath', $arr_new);
 	//print_r($arr2);	
+	//print_r($arr_times_of_day_nums);
+	/*
+echo $today_date.'<br>';
+echo $tomorrow_date.'<br>';
+echo $after_tomorrow_date.'<br>';
+
+	
+	print_r($today);
+	
+	print_r($tomorrow);
+	
+	print_r($after_tomorrow);
 	?>
+	*/
+	
 	
 
 
