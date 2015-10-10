1833610b5eb3ed590ad3ec512d47a4583665348b
vosaan <vo.saan@gmail.com>
2015-10-10 23:49:29 +0300

    валидируется регистрация

--- themes/theme01/templates/validator.js
+++ themes/theme01/templates/validator.js
@@ -1,4 +1,9 @@
-function validate(){
+var err_mess1 = "Это поле должно быть заполнено!";
+var err_mess2 = "Поле имеет неверный формат! ";
+var err_mess3 = "Пароли не совпадают!";
+
+
+function feed_validate(){
   var valide = true;
   
   /* Валидация формы обратной связи */
@@ -6,7 +11,7 @@ function validate(){
   /* Если поле "Заголовок" не заполнено, выводится соотв. сообщение в блок с id=title_error */
   var t_error = document.getElementById("title_error");
   if(document.feedback.title.value == ""){
-    t_error.innerHTML = "Это поле должно быть заполнено!";
+    t_error.innerHTML = err_mess1;
     valide = false;
   }
 
@@ -18,7 +23,7 @@ function validate(){
    */
   var email_error = document.getElementById("email_error");
   if(document.feedback.email.value == ""){
-    email_error.innerHTML = "Это поле должно быть заполнено!";
+    email_error.innerHTML = err_mess1;
     valide = false;
   } else if (!document.feedback.email.value.match(reg)){
     email_error.innerHTML = "Неверный формат e-mail"; 
@@ -28,7 +33,7 @@ function validate(){
   /* Если поле "Отзыв" не заполнено, выводится соотв. сообщение в блок с id=message_error */
   var m_error = document.getElementById("message_error");
   if(document.feedback.message.value == ""){
-    m_error.innerHTML = "Это поле должно быть заполнено!";
+    m_error.innerHTML = err_mess1;
     valide = false;
   }
 
@@ -37,22 +42,38 @@ function validate(){
 
 function reg_validate(){
   var reg_valide = true;
-  var err_mess1 = "Это поле должно быть заполнено!"
-
+  reg_login = /^[a-z0-9_-]{3,15}$/;
+  reg_password = /^[a-z0-9_-]{6,15}$/;
+  
   if(document.reg.reg_form_login.value == ""){
     document.getElementById("reg_l_error").innerHTML = err_mess1;
+    reg_valide = false;
+  } else if(!document.reg.reg_form_login.value.match(reg_login)){
+    document.getElementById("reg_l_error").innerHTML = err_mess2;
     reg_valide = false;
   }
 
   if(document.reg.reg_form_password.value == ""){
     document.getElementById("reg_p_error").innerHTML = err_mess1;
     reg_valide = false;
+  } else if(!document.reg.reg_form_password.value.match(reg_password)){
+    document.getElementById("reg_p_error").innerHTML = err_mess2;
+    reg_valide = false;
   }
 
   if(document.reg.reg_form_password_confirm.value == ""){
     document.getElementById("reg_p_c_error").innerHTML = err_mess1;
     reg_valide = false;
-  }   
+  } else if(!document.reg.reg_form_password_confirm.value.match(reg_password)){
+    document.getElementById("reg_p_c_error").innerHTML = err_mess2;
+    reg_valide = false;
+  }
+
+  if(document.reg.reg_form_password.value !== document.reg.reg_form_password_confirm.value){
+    document.getElementById("reg_p_c_error").innerHTML = err_mess3;
+    document.getElementById("reg_p_error").innerHTML = err_mess3;
+    reg_valide = false;
+  }  
 
   return reg_valide;
 }
