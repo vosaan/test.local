9fc301df1d5058f2759b8b2e6758fe1bf615d73c
vosaan <vo.saan@gmail.com>
2015-10-10 18:45:12 +0300

    валидация формы обратной связи

--- themes/theme01/templates/validator.js
+++ themes/theme01/templates/validator.js
@@ -1,131 +1,28 @@
-$(document).ready(function(){
+  function validate(){
+    var valide = true;
+    
+    var t_error = document.getElementById("title_error");
+    if(document.feedback.title.value == ""){
+      t_error.innerHTML = "Это поле должно быть заполнено!";
+      valide = false;
+    }
 
-   // Устанавливаем обработчик потери фокуса для всех полей ввода текста
-   $('input#name, input#email, textarea#message').unbind().blur( function(){
+    reg = /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,12})$/;
+    
+    var email_error = document.getElementById("email_error");
+    if(document.feedback.email.value == ""){
+      email_error.innerHTML = "Это поле должно быть заполнено!";
+      valide = false;
+    } else if (!document.feedback.email.value.match(reg)){
+      email_error.innerHTML = "Неверный формат e-mail"; 
+      valide = false; 
+    }
+    
+    var m_error = document.getElementById("message_error");
+    if(document.feedback.message.value == ""){
+      m_error.innerHTML = "Это поле должно быть заполнено!";
+      valide = false;
+    }
 
-      // Для удобства записываем обращения к атрибуту и значению каждого поля в переменные
-       var id = $(this).attr('id');
-       var val = $(this).val();
-
-     // После того, как поле потеряло фокус, перебираем значения id, совпадающие с id данного поля
-     switch(id)
-     {
-           // Проверка поля "Имя"
-           case 'name':
-              var rv_name = /^[a-zA-Zа-яА-Я]+$/; // используем регулярное выражение
-
-              // Eсли длина имени больше 2 символов, оно не пустое и удовлетворяет рег. выражению,
-              // то добавляем этому полю класс .not_error,
-              // и ниже в контейнер для ошибок выводим слово " Принято", т.е. валидация для этого поля пройдена успешно
-
-              if(val.length > 2 && val != '' && rv_name.test(val))
-              {
-                 $(this).addClass('not_error');
-                 $(this).next('.error-box').text('Принято')
-                                           .css('color','green')
-                                           .animate({'paddingLeft':'10px'},400)
-                                           .animate({'paddingLeft':'5px'},400);
-              }
-
-            // Иначе, мы удаляем класс not-error и заменяем его на класс error, говоря о том что поле содержит ошибку валидации,
-            // и ниже в наш контейнер выводим сообщение об ошибке и параметры для верной валидации
-
-              else
-              {
-                 $(this).removeClass('not_error').addClass('error');
-                 $(this).next('.error-box').html('поле "Имя" обязательно для заполнения<br>, длина имени должна составлять не менее 2 символов<br>, поле должно содержать только русские или латинские буквы')
-                                            .css('color','red')
-                                            .animate({'paddingLeft':'10px'},400)
-                                            .animate({'paddingLeft':'5px'},400);
-              }
-          break;
-
-         // Проверка email
-         case 'email':
-             var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
-             if(val != '' && rv_email.test(val))
-             {
-                $(this).addClass('not_error');
-                $(this).next('.error-box').text('Принято')
-                                          .css('color','green')
-                                          .animate({'paddingLeft':'10px'},400)
-                                          .animate({'paddingLeft':'5px'},400);
-             }
-             else
-             {
-                $(this).removeClass('not_error').addClass('error');
-                $(this).next('.error-box').html('поле "Email" обязательно для заполнения<br>, поле должно содержать правильный email-адрес<br>
-                                           .css('color','red')
-                                           .animate({'paddingLeft':'10px'},400)
-                                           .animate({'paddingLeft':'5px'},400);
-             }
-         break;
-
-
-        // Проверка поля "Сообщение"
-        case 'message':
-            if(val != '' && val.length < 5000)
-            {
-               $(this).addClass('not_error');
-               $(this).next('.error-box').text('Принято').
-                                         .css('color','green')
-                                         .animate({'paddingLeft':'10px'},400)
-                                         .animate({'paddingLeft':'5px'},400);
-            }
-            else
-            {
-               $(this).removeClass('not_error').addClass('error');
-               $(this).next('.error-box').html('поле "Текст письма" обязательно для заполнения')
-                                         .css('color','red')
-                                         .animate({'paddingLeft':'10px'},400)
-                                         .animate({'paddingLeft':'5px'},400);
-            }
-        break;
-
-     } // end switch(...)
-
-   }); // end blur()
-
-   // Теперь отправим наше письмо с помощью AJAX
-   $('form#feedback-form').submit(function(e){
-
-       // Запрещаем стандартное поведение для кнопки submit
-       e.preventDefault();
-
-       // После того, как мы нажали кнопку "Отправить", делаем проверку,
-       // если кол-во полей с классом .not_error равно 3 (так как у нас всего 3 поля), то есть все поля заполнены верно,
-       // выполняем наш Ajax сценарий и отправляем письмо адресату
-
-       if($('.not_error').length == 3)
-       {
-          // Eще одним моментом является то, что в качестве указания данных для передачи обработчику send.php, мы обращаемся $(this) к нашей форме,
-          // и вызываем метод .serialize().
-          // Это очень удобно, т.к. он сразу возвращает сгенерированную строку с именами и значениями выбранных элементов формы.
-
-           $.ajax({
-                  url: 'send.php',
-                  type: 'post',
-                  data: $(this).serialize(),
-
-                  beforeSend: function(xhr, textStatus){ 
-                       $('form#feedback-form :input').attr('disabled','disabled');
-                  },
-
-                 success: function(response){
-                      $('form#feedback-form :input').removeAttr('disabled');
-                      $('form#feedback-form :text, textarea').val('').removeClass().next('.error-box').text('');
-                      alert(response);
-                 }
-          }); // end ajax({...})
-      }
-
-      // Иначе, если количество полей с данным классом не равно значению 3, мы возвращаем false,
-      // останавливая отправку сообщения в невалидной форме
-     else
-     {
-        return false;
-     }
-
- }); // end submit()
-
-}); // end script
+    return valide;
+  }
